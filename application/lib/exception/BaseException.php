46fd6bdb4b51d5c4ff67ee67f174d1bfc111d6c2
xylong <41631808@qq.com>
2018-05-12 18:33:05 +0800

    添加构造函数接收code，errorCode，msg

--- application/lib/exception/BaseException.php
+++ application/lib/exception/BaseException.php
@@ -7,7 +7,6 @@ use think\Exception;
 */
 class BaseException extends Exception
 {
-	
 	// http 状态码
 	public $code = 400;
 
@@ -16,4 +15,23 @@ class BaseException extends Exception
 	
 	// 错误信息
 	public $msg = '参数错误';
+
+	public function __construct($params=[])
+	{
+		if (!is_array($params)) {
+			return;
+		}
+
+		if (array_key_exists('code', $params) && $params['code']) {
+			$this->code = $params['code'];
+		}
+
+		if (array_key_exists('errorCode', $params) && $params['errorCode']) {
+			$this->errorCode = $params['errorCode'];
+		}
+
+		if (array_key_exists('msg', $params) && $params['msg']) {
+			$this->msg = $params['msg'];
+		}
+	}
 }
