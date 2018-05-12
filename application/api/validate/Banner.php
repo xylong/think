7d8b2297d015b26a741f57e75af0ee5b776030c1
xylong <41631808@qq.com>
2018-05-12 22:26:55 +0800

    子类重载父类验证规则

--- application/api/validate/Banner.php
+++ application/api/validate/Banner.php
@@ -5,4 +5,20 @@ use think\Validate;
 
 class Banner extends BaseValidate
 {
+	public function __construct()
+	{
+		parent::__construct();
+
+		// 重载规则
+		$this->rule = [
+			'id' => ['require', 'checkId'],
+			'type' => ['in' => ['new', 'hot']],
+		];
+
+		$this->message = [
+			'type.in' => 'type类型错误',
+			'id.require' => 'id必须',
+        	'id.checkId'   => 'id必须是正整数',
+		];
+	}
 }
