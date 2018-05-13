e6f7b4aab6ea06271f1e12cdab573c677c9c75c8
xylong <41631808@qq.com>
2018-05-13 22:23:24 +0800

    API基类

---
+++ application/api/controller/Base.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\api\controller;
+
+use think\Controller;
+use app\common\controller\Common;
+
+
+class Base extends Common
+{
+	protected function initialize()
+    {
+        parent::initialize();
+    }
+
+    public function login()
+    {
+    }
+
+    public function logout()
+    {
+    }
+
+    public function miss()
+    {
+    }
+}
