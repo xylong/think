379dd0b4e90a81a6cfbbc33eed63c69bfdc3e980
xylong <41631808@qq.com>
2018-05-20 14:23:09 +0800

    设置场景验证

--- application/api/validate/Product.php
+++ application/api/validate/Product.php
@@ -3,17 +3,29 @@ namespace app\api\validate;
 
 class Product extends BaseValidate
 {
-	public function __construct()
+	protected $scene = [
+		// 最新
+        'recent'  =>  ['count'],
+        // 分类
+        'category' => ['category_id'],
+    ];
+
+	public function __construct($scene='')
 	{
 		parent::__construct();
 
 		$this->rule = [
+			'category_id'	=> ['require', 'checkId'],
 			'count' => 'checkId|between:1,15'
 		];
 
 		$this->message = [
+			'category_id.require'	=> 'id必须',
+			'category_id.checkId'	=> 'category_id必须是正整数',
 			'count.checkId' => 'count必须是正整数',
 			'count.between' => 'count必须在1-15之间'
 		];
+
+    	$this->scene_name = $scene;
 	}
 }
