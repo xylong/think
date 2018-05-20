8c77e4c67920a1e728cdbe6b4f48688502e9015c
xylong <41631808@qq.com>
2018-05-20 12:05:33 +0800

    商品

---
+++ application/lib/exception/ProductException.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\lib\exception;
+
+/**
+* 
+*/
+class ProductException extends BaseException
+{
+	
+	public $code = 404;
+
+	public $errorCode = 20000;
+	
+	public $msg = '指定商品不存在';
+}
