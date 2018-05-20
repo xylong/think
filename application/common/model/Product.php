c538a69996efe9aad565d57033f05de1585d3a37
xylong <41631808@qq.com>
2018-05-20 10:46:34 +0800

    隐藏pivot，处理冗余字段imgurl地址

--- application/common/model/Product.php
+++ application/common/model/Product.php
@@ -4,4 +4,10 @@ namespace app\common\model;
 
 class Product extends BaseModel
 {
+	protected $hidden = ['pivot', 'create_time', 'update_time', 'delete_time', 'from', 'category_id'];
+
+	public function getMainImgUrlAttr($value, $data)
+    {
+    	return $this->prefixImgUrl($value, $data);
+    }
 }
