4b74b049168f1e6fd36dca3a5dcf6627f5fcbb03
xylong <41631808@qq.com>
2018-05-20 12:04:56 +0800

    商品验证器

--- application/api/validate/Product.php
+++ application/api/validate/Product.php
@@ -6,5 +6,14 @@ class Product extends BaseValidate
 	public function __construct()
 	{
 		parent::__construct();
+
+		$this->rule = [
+			'count' => 'checkId|between:1,15'
+		];
+
+		$this->message = [
+			'count.checkId' => 'count必须是正整数',
+			'count.between' => 'count必须在1-15之间'
+		];
 	}
 }
