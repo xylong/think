bbfd825d40c988ed0e5108ccd7c9adebda57ff5b
xylong <41631808@qq.com>
2018-05-20 12:04:42 +0800

    修改类名

--- application/api/controller/v1/Product.php
+++ application/api/controller/v1/Product.php
@@ -1,11 +1,26 @@
 <?php
+
 namespace app\api\controller\v1;
 
 use app\common\model\Product as ProductModel;
 use app\api\validate\Product as ProductValidate;
-use app\lib\exception\ProductMissException;
+use app\lib\exception\ProductException;
 
+/**
+ * 商品
+ */
 class Product extends Base
 {
-    
+    public function getRecent($count=15)
+    {
+        (new ProductValidate)->_check();
+
+        $products = ProductModel::getMostRecent($count);
+
+        if ($products->isEmpty()) {
+            throw new ProductException;
+        }
+
+        return $products;
+    }
 }
