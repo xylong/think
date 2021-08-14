f0e8af0a09444adebfd9806cac13298a84f6c163
xyl <416319808@qq.com>
2021-08-14 21:17:03 +0800

    fix 关闭channel

--- goroutine/sum.go
+++ goroutine/sum.go
@@ -2,13 +2,14 @@ package goroutine
 
 import (
 	"fmt"
+	"sync"
 	"think/factory"
 )
 
 var (
 	min   = 1
 	max   = 100000000
-	group = 10
+	group = 2
 )
 
 type Sum struct {
@@ -31,7 +32,8 @@ func (s *Sum) sum1(min, max int) int {
 
 // sum2	求和
 func (s *Sum) sum2(min, max int) {
-	s.c <- s.sum1(min, max)
+	r := s.sum1(min, max)
+	s.c <- r
 }
 
 func (s *Sum) Run() {
@@ -45,17 +47,25 @@ func (s *Sum) Run() {
 func (s *Sum) Go() {
 	s.Start()
 	s.c = make(chan int, group)
+	wg := sync.WaitGroup{}
 
 	result := 0
+	wg.Add(group)
 	for i := 0; i < group; i++ {
-		n := max / group
-		s.sum2(n*i, n*i+n)
+		go func(i int) {
+			defer wg.Done()
+			n := max / group
+			s.sum2(n*i, n*i+n)
+		}(i)
 	}
-	// 关闭channel，不然range会死锁
-	close(s.c)
 
-	for r := range s.c {
-		result = result + r
+	go func() {
+		defer close(s.c)
+		wg.Wait()
+	}()
+
+	for v := range s.c {
+		result = result + v
 	}
 
 	s.End()
