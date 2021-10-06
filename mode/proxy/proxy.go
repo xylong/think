bd21fad08dc970d2c9d02938c65eaca561ce2de4
xyl <416319808@qq.com>
2021-10-06 11:20:29 +0800

    调整

--- mode/proxy/proxy.go
+++ mode/proxy/proxy.go
@@ -2,30 +2,32 @@ package proxy
 
 import "fmt"
 
-// UserService 用户服务
-type UserService struct{}
+type (
+	// Login 登录方法
+	Login func(name, password string)
 
-// LoginFunc 登录方法
-type LoginFunc func(name, password string)
+	// LoginLog 登录日志,装饰登录，记录日志
+	LoginLog func(Login) Login
+)
 
-// LogDecorator 日志装饰器
-type LogDecorator func(LoginFunc) LoginFunc
-
-// Log2Mongo 记录mongo日志
-func Log2Mongo(lf LoginFunc) LoginFunc {
+// Log2Mongo 记录mongodb日志
+func Log2Mongo(l Login) Login {
 	return func(name, password string) {
-		lf(name, password)
-		fmt.Println("日志记录到mongodb")
+		l(name, password)
+		fmt.Println("记录到mongodb")
 	}
 }
 
 // Log2Mysql 记录mysql日志
-func Log2Mysql(lf LoginFunc) LoginFunc {
+func Log2Mysql(l Login) Login {
 	return func(name, password string) {
-		lf(name, password)
-		fmt.Println("日志记录到mysql")
+		l(name, password)
+		fmt.Println("记录到mysql")
 	}
 }
+
+// UserService 用户服务
+type UserService struct{}
 
 // Login 登录
 func (us *UserService) Login(name, password string) {
@@ -48,6 +50,6 @@ func (us *UserProxy) Login(name, password string) {
 }
 
 // Login2 代理+装饰器
-func (us *UserProxy) Login2(ld LogDecorator) LoginFunc {
-	return ld(us.Login)
+func (us *UserProxy) Login2(ll LoginLog) Login {
+	return ll(us.Login)
 }
