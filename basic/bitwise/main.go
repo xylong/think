d80edd7287ce413c7c768f616c249087ecbb046a
xyl <416319808@qq.com>
2024-10-11 23:59:39 +0800

    位运算实现权限组合

--- basic/bitwise/main.go
+++ basic/bitwise/main.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 )
 
+// 位运算实现权限组合
 const (
 	Read   int64 = 1 // 0001
 	Create int64 = 2 // 0010
@@ -15,6 +16,22 @@ const (
 // 判断是否有某个权限
 func hasPermission(role, permission int64) bool {
 	return role&permission == permission
+}
+
+// showPermission 查看有哪些权限
+func showPermission(role int64) []int64 {
+	var (
+		arr = []int64{Read, Create, Update, Delete}
+		brr []int64
+	)
+
+	for _, i := range arr {
+		if hasPermission(role, i) {
+			brr = append(brr, i)
+		}
+	}
+
+	return brr
 }
 
 func main() {
@@ -30,4 +47,7 @@ func main() {
 	Writer = Writer ^ Delete
 	fmt.Println(Writer, strconv.FormatInt(Writer, 2))
 	fmt.Printf("删除权限：%v\n", hasPermission(Writer, Delete))
+
+	// 查看有哪些权限
+	fmt.Println(showPermission(Writer))
 }
